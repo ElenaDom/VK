603e09fe43efa0116b53b036cd37562f9adaa8c0
Елена Домнина <Elena@MacProElena.local>
2020-12-06 22:22:51 +0400

    init

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+public class Post {
+    private int id;
+    private int date;
+    private int image;
+    private int note;
+    private int firstname;
+    private int lastname;
+    private int message;
+    private int friend;
+
+
+}
