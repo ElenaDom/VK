c4d01af7365a4d61633ca48e408424adf8f75637
Елена Домнина <Elena@MacProElena.local>
2020-12-07 17:34:24 +0400

    manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,13 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int overID, String domain, String query, boolean ownersOnly, int count, int offset, boolean extended, String fields) {
+        return null;
+    }
+    public void delete(int overID, int postID){
+    }
+}
