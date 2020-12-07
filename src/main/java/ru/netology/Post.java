6b806fbe36e3928359d9a42a58d9a39814e00944
Елена Домнина <Elena@MacProElena.local>
2020-12-07 15:31:34 +0400

    Several fields added

--- src/main/java/ru/netology/Post.java
+++ src/main/java/ru/netology/Post.java
@@ -9,6 +9,107 @@ public class Post {
     private int lastname;
     private int message;
     private int friend;
+    private String likesCount;
+    private String messagesInfo;
+    private String storiesInfo;
+    private String player;
 
+
+
+    public String getMessagesInfo() {
+        return messagesInfo;
+    }
+
+    public void setMessagesInfo(String messagesInfo) {
+        this.messagesInfo = messagesInfo;
+    }
+
+    public String getStoriesInfo() {
+        return storiesInfo;
+    }
+
+    public void setStoriesInfo(String storiesInfo) {
+        this.storiesInfo = storiesInfo;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+
+    public String getLikesCount() {
+        return likesCount;
+    }
+
+    public void setLikesCount(String likesCount) {
+        this.likesCount = likesCount;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNote() {
+        return note;
+    }
+
+    public void setNote(int note) {
+        this.note = note;
+    }
+
+    public int getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(int firstname) {
+        this.firstname = firstname;
+    }
+
+    public int getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(int lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getFriend() {
+        return friend;
+    }
+
+    public void setFriend(int friend) {
+        this.friend = friend;
+    }
+
+    public int getMessage() {
+        return message;
+    }
+
+    public void setMessage(int message) {
+        this.message = message;
+    }
 
 }
